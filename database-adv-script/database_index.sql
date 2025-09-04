3effb732489ca6c2f977d54d7fa2f2ba2c277fdd
salmalah <l.salmalahmidi@gmail.com>
2025-09-04 14:24:22 +0100

    Add CREATE INDEX commands and EXPLAIN ANALYZE queries for performance measurement

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -3,4 +3,6 @@ CREATE INDEX idx_bookings_property_id ON bookings(property_id);
 CREATE INDEX idx_bookings_start_date ON bookings(start_date);
 CREATE INDEX idx_users_name ON users(name);
 CREATE INDEX idx_properties_name ON properties(name);
-
+-- Measure performance before and after adding indexes
+EXPLAIN ANALYZE SELECT * FROM bookings b JOIN users u ON b.user_id = u.id WHERE b.start_date > '2025-01-01';
+EXPLAIN ANALYZE SELECT * FROM bookings b JOIN properties p ON b.property_id = p.id WHERE b.start_date > '2025-01-01';
