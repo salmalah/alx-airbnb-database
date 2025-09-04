b98bf85a2bd8a16ab043179c6969f69855bf4fbc
salmalah <l.salmalahmidi@gmail.com>
2025-09-04 14:18:09 +0100

    Fix window function query to use ROW_NUMBER()

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -11,7 +11,7 @@ ORDER BY total_bookings DESC;
 SELECT p.id AS property_id,
        p.name AS property_name,
        COUNT(b.id) AS total_bookings,
-       RANK() OVER (ORDER BY COUNT(b.id) DESC) AS booking_rank
+       ROW_NUMBER() OVER (ORDER BY COUNT(b.id) DESC) AS booking_rank
 FROM properties p
 LEFT JOIN bookings b ON p.id = b.property_id
 GROUP BY p.id, p.name
